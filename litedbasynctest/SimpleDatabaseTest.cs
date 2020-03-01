ec441214c6513ef62d2305c69a2c5b8fdf3b9133
Mark Lockett <mark@deliveryengine.net>
2020-03-01 13:03:15 +0800

    Add a async wrapper for the LiteDB Query type plus change unit tests

--- litedbasynctest/SimpleDatabaseTest.cs
+++ litedbasynctest/SimpleDatabaseTest.cs
@@ -32,7 +32,7 @@ namespace litedbasynctest
             var upsertResult = await collection.UpsertAsync(person);
             Assert.True(upsertResult);
 
-            var listResult = await collection.ToListAsync();
+            var listResult = await collection.QueryAsync().ToListAsync();
             Assert.Single(listResult);
             var resultPerson = listResult[0];
             Assert.Equal(person.Id, resultPerson.Id);
@@ -58,7 +58,7 @@ namespace litedbasynctest
             Assert.True(insertResult.IsGuid);
             Assert.Equal(person.Id, insertResult.AsGuid);
 
-            var listResult = await collection.ToListAsync();
+            var listResult = await collection.QueryAsync().ToListAsync();
             Assert.Single(listResult);
             var resultPerson = listResult[0];
             Assert.Equal(person.Id, resultPerson.Id);
